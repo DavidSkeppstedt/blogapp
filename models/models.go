73e72d1c8a6b906e946216672a24112a68a77ac0
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-06 21:45:26 +0100

    changed a letter from capital to lower case as to adhere to the json convention.

--- models/models.go
+++ models/models.go
@@ -7,7 +7,7 @@ import (
 type Post struct {
 	Id        int       `json:"id"`
 	Title     string    `json:"title"`
-	Body      string    `json:"Body"`
+	Body      string    `json:"body"`
 	Author    string    `json:"author"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
