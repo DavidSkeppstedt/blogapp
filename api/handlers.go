1dc5e64ad0e3b15446dc9ef55b14271bb6ecea41
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-06 19:23:54 +0100

    Implementedt the postsHandler, which is responding to the /api/1/posts endpoint. It returns the posts resource back to the client.

--- api/handlers.go
+++ api/handlers.go
@@ -2,14 +2,30 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/DavidSkeppstedt/blog/database"
+	"github.com/DavidSkeppstedt/blog/models"
+	_ "github.com/gorilla/context"
+	_ "github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 type appContext struct {
+	db *database.DbConn
 }
 
-func (c *appContext) helloHandler(w http.ResponseWriter, r *http.Request) {
+func (c *appContext) postsHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	var count string
+	if len(params) > 0 && params.Get("count") != "" {
+		count = params.Get("count")
+	} else {
+		count = "100"
+	}
+	responseData, err := c.db.ListPosts(count)
+	if err != nil {
+		panic(err)
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("Hello World")
+	json.NewEncoder(w).Encode(&models.PostsCollection{responseData})
 }
