902efd5f06ae12bb0913126dbf24d7f61df830c5
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-06 19:22:16 +0100

    Updated the database with a new Open function that returns an wraped type DbConn which now also have funcitons bound to it.

--- database/db.go
+++ database/db.go
@@ -3,21 +3,46 @@ package database
 import (
 	"database/sql"
 	"github.com/DavidSkeppstedt/blog/credentialsutil"
+	"github.com/DavidSkeppstedt/blog/models"
 	_ "github.com/lib/pq"
 	"log"
+	"strconv"
 )
 
-func Open() (db *sql.DB, err error) {
+type DbConn struct {
+	db *sql.DB
+}
+
+func Open() (conn *DbConn, err error) {
 	dbConf, err := credentialsutil.CreateConfig("config/db.conf")
 	if err != nil {
 		log.Println("Error with database config", err.Error())
 		panic(err)
 	}
-	db, err = sql.Open("postgres", dbConf)
+	db, err := sql.Open("postgres", dbConf)
 
 	if err != nil {
 		log.Println("Problem with the database driver", err.Error())
 		panic(err)
 	}
-	return db, err
+	conn = &DbConn{db}
+	return conn, err
+}
+
+func (this *DbConn) ListPosts(count string) (result []models.Post, err error) {
+	limit, err := strconv.Atoi(count)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := this.db.Query("SELECT * FROM posts LIMIT $1", limit)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var post models.Post
+		rows.Scan(&post.Id, &post.Title, &post.Body, &post.Author,
+			&post.CreatedAt, &post.UpdatedAt)
+		result = append(result, post)
+	}
+	return result, err
 }
