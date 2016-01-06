66e4dd19533cfc0644a8946abb1bc2020f74fea3
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-06 19:21:25 +0100

    Added the Post model and the Posts collection model.

---
+++ models/models.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+)
+
+type Post struct {
+	Id        int       `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"Body"`
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+type PostsCollection struct {
+	Data []Post `json:"data"`
+}
