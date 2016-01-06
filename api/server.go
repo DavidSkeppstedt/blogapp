afcc0adf1357b10308380ea47ef21ab424af2e59
David Skeppstedt <david.skeppstedt@gmail.com>
2016-01-06 19:25:35 +0100

    Updated the Start function so that it creates a appContext with a connection to the database and creates a new router.

--- api/server.go
+++ api/server.go
@@ -1,10 +1,17 @@
 package api
 
 import (
+	"github.com/DavidSkeppstedt/blog/database"
 	"net/http"
 )
 
 func Start() {
-	router := NewRouter()
+	dbConn, err := database.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	appContext := &appContext{dbConn}
+	router := NewRouter(appContext)
 	http.ListenAndServe(":8080", router)
 }
